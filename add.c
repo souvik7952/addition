5f7318f91a6d9002a08365c9f372c6b9edb6c279
naveenreddy2000 <konugantinaveenreddy@gmail.com>
2019-05-29 19:35:33 +0530

    modified

--- add.c
+++ add.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 main()
 {
-    int a,b;
+    int a,b,c;
     printf("enter 2 numbers");
     scanf("%d%d",&a,&b);
-    printf("sum of numbers is %d",a+b);
+    c = a+ b ;
+    printf("sum of numbers is %d",c);
 }
